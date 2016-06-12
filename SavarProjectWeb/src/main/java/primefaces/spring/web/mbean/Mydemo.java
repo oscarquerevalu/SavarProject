11b16d90de1c10137e29fd18ca92a85345154460
Oscar Querevalu <oscarquerevalu@gmail.com>
2016-06-12 18:56:57 -0500

    config

--- SavarProjectWeb/src/main/java/primefaces/spring/web/mbean/Mydemo.java
+++ SavarProjectWeb/src/main/java/primefaces/spring/web/mbean/Mydemo.java
@@ -6,7 +6,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
+import com.iop.SavarProjectService.dto.Producto;
 import com.iop.SavarProjectService.service.AlmacenService;
+import com.iop.SavarProjectService.service.ProductoService;
 
 @Component
 public class Mydemo {
@@ -15,6 +17,10 @@ public class Mydemo {
 	@Autowired(required=true)
 	@Qualifier(value="almacenService")
     private AlmacenService  myService; 
+	
+	@Autowired(required=true)
+	@Qualifier(value="productoService")
+    private ProductoService  productoService; 
 
     /**
      * @param args
@@ -36,7 +42,18 @@ public class Mydemo {
         // TODO Auto-generated method stub
         System.out.println("---callService---");
         System.out.println(myService);
-        myService.getAlmacenById("");
+//        
+//        Producto producto = new Producto();
+//        producto.setIdProducto("1");
+//        producto.setNombre_producto("Pan");
+//        producto.setVolumen_m3(22.2);
+//        producto.setCantidad_producto(20);
+//        
+//        productoService.addProducto(producto);
+//        productoService.getProductoById("1");
+        System.out.println("NextId>>> "+myService.getNextId());
+        
+        
 
     }
 
